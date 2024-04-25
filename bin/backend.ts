533924f3511e61127c15e85adadd8f4b1eb3d410
hazeliscoding <duck_soop@outlook.com>
2024-04-24 19:10:30 -0500

    feat: 🎸 create compute stack

--- bin/backend.ts
+++ bin/backend.ts
@@ -2,6 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DatabaseStack } from '../lib/database-stack';
+import { ComputeStack } from '../lib/compute-stack';
 
 const app = new cdk.App();
 const dbStack = new DatabaseStack(app, 'DatabaseStack');
+const computeStack = new ComputeStack(app, 'ComputeStack', {
+  usersTable: dbStack.usersTable,
+});
---
+++ lib/compute-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+import path = require('path');
+
+interface ComputeStackProps extends cdk.StackProps {
+  usersTable: Table;
+}
+
+export class ComputeStack extends cdk.Stack {
+  readonly addUserToTableFunc: NodejsFunction;
+
+  constructor(scope: Construct, id: string, props: ComputeStackProps) {
+    super(scope, id, props);
+
+    this.addUserToTableFunc = this.addUserToUsersTable(props);
+  }
+
+  addUserToUsersTable(props: ComputeStackProps) {
+    const func = new NodejsFunction(this, 'addUserFunc', {
+      functionName: 'addUserFun',
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: path.join(
+        __dirname,
+        '../functions/AddUserPostConfirmation/index.ts'
+      ),
+    });
+
+    func.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['dynamodb:PutItem'],
+        resources: [props.usersTable.tableArn as string],
+      })
+    );
+
+    return func;
+  }
+}
